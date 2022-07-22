99eca9aa93d6bf555e4bf03135433605c67955f5
a12345645 <a12345645kimo.com@gmail.com>
2022-07-22 16:57:37 +0800

    Stop service

--- remove.sh
+++ remove.sh
@@ -2,6 +2,7 @@
 systemctl list-unit-files | grep watchdog.service
 if [ "$?" -eq 0 ]; then
     systemctl disable watchdog.service
+    systemctl stop watchdog.service
     rm /etc/systemd/system/watchdog.service
     systemctl daemon-reload
     echo  -e "\033[32m Complete!\033[0m"
