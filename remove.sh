121552d4f6b3b3f9a74ffff29e0ee8a0aaadcc5e
a12345645 <a12345645kimo.com@gmail.com>
2022-07-22 16:01:15 +0800

    Typeset

--- remove.sh
+++ remove.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 systemctl list-unit-files | grep watchdog.service
 if [ "$?" -eq 0 ]; then
-systemctl disable watchdog.service
-rm /etc/systemd/system/watchdog.service
-systemctl daemon-reload
-echo  -e "\033[32m Complete!\033[0m"
+    systemctl disable watchdog.service
+    rm /etc/systemd/system/watchdog.service
+    systemctl daemon-reload
+    echo  -e "\033[32m Complete!\033[0m"
 else
-echo  -e "\033[31m watchdog service isn't exist.\033[0m"
+    echo  -e "\033[31m watchdog service isn't exist.\033[0m"
 fi
